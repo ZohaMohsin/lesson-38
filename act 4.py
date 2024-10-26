df4d5a225d4f425cf55963da6ab75de2fe59b7c6
ZohaMohsin <amnamohsin72@gmail.com>
2024-10-26 21:44:26 +0500

    project

---
+++ act 4.py
@@ -0,0 +1,11 @@
+#swapping of numbers
+a=10
+b=20
+
+print("before swapping values of a is ",a," and b is ",b)
+
+c=a
+a=b
+b=c
+
+print("after swapping valus of a is ",a," and b is",b)
---
+++ project.py
@@ -0,0 +1,11 @@
+#swapping of numbers
+a=10
+b=20
+c=30
+print("before swapping valus of a is ",a," and b is ",b," and c is ",c)
+
+c=a
+a=b
+b=c
+
+print(" after swapping valus of a is ",a," and b is ",b," and c is ",c)
